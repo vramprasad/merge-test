df2417c543d2b71180124097f94f78e0c1b4de60
Ramprasad V <vrprasad@gmail.com>
2022-09-20 19:17:09 +0530

    Added base code

---
+++ src/main/java/com/prasad/HelloAPIApplication.java
@@ -0,0 +1,11 @@
+package com.prasad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloAPIApplication.class, args);
+    }
+}
---
+++ src/main/java/com/prasad/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.prasad.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import  org.springframework.web.bind.annotation.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/healthcheck",method = RequestMethod.GET, produces={MediaType.TEXT_PLAIN_VALUE})
+    @ResponseBody
+    public ResponseEntity<String> healthcheck() {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String responseText = "merge-test-api Healthcheck @ "+ timeStamp+" - All OK";
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body(responseText.toString());
+    }
+
+
+
+}
